b25f24790422da48462d31ba553a4788ec1918d8
ibll <61601241+ibll@users.noreply.github.com>
2024-03-26 12:54:19 -0500

    watchusay

--- dot_oh-my-zsh/custom/exports.zsh
+++ dot_oh-my-zsh/custom/exports.zsh
@@ -1,5 +1,8 @@
 # Sets default editor
 export EDITOR="nvim"
+export VISUAL="nvim"
+
+export MANPAGER="/bin/sh -c \"col -b | vim -c 'set ft=man ts=8 nomod nolist nonu noma' -\""
 
 # Adds Nim to path
 export PATH=/Users/isbell/.nimble/bin:$PATH
