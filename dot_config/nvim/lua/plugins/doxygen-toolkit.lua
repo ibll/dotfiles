396773e75d6556ac11452a07c6c1784706b2b56f
ibll <61601241+ibll@users.noreply.github.com>
2025-09-10 20:19:33 -0500

    removed old nvim stuff

--- dot_config/nvim/lua/plugins/doxygen-toolkit.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "vim-scripts/DoxygenToolkit.vim",
-  config = function()
-    vim.g.DoxygenToolkit_authorName = "Your Name"
-    vim.g.DoxygenToolkit_briefTag_pre = "@brief "
-    vim.g.DoxygenToolkit_paramTag_pre = "@param "
-    vim.g.DoxygenToolkit_returnTag = "@return "
-  end,
-}
