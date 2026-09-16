8db7edcc68ebd6b5636939f4dd6c389a3001b809
ibll <61601241+ibll@users.noreply.github.com>
2026-09-15 19:24:24 -0500

    Remove problematic python path export

--- dot_oh-my-zsh/custom/exports.zsh
+++ dot_oh-my-zsh/custom/exports.zsh
@@ -6,7 +6,6 @@ export VISUAL="nvim"
 export PATH=/Users/isbell/.nimble/bin:$PATH
 export PATH=/Users/isbell/.cargo/bin:$PATH
 export PATH=/Users/isbell/bin:$PATH
-export PATH="/Library/Frameworks/Python.framework/Versions/3.11/bin:${PATH}"
 export PATH="$PATH:/Users/isbell/.local/bin"
 
 # Bitwarden SSH
